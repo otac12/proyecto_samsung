bc74ea63302933e6f0ee990aa93ebab1319bda50
otac12 <otac@hotmail.com>
2023-11-03 20:15:19 -0600

    Enganche4

--- Enganche.py
+++ Enganche.py
@@ -1,12 +1,19 @@
-
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+print("Se movera motor")
 
-while True:
-    servo.min()
-    sleep(2)
-    servo.max()
+servo= AngularServo(17,min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+while (True):
+    respuesta=input("Cerrar o abrir")
+
+    if respuesta=="Cerrar":
+        servo.angle=180
+    
+    if respuesta=="abrir":
+        servo.angle=0
+
+
 
 
