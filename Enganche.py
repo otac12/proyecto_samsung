a184667721b120759575dc4a11795c34bad6ef0d
otac12 <otac@hotmail.com>
2023-11-03 19:13:15 -0600

    Enganche4

--- Enganche.py
+++ Enganche.py
@@ -4,11 +4,16 @@ from time import sleep
 print("Se movera motor")
 
 servo= AngularServo(17)
+myCorrection=0.45
+maxPW=(2.0+myCorrection)/1000
+minPW=(1.0-myCorrection)/1000
+
+print("Max:"+ str(maxPW)+"\n min:"+ str(minPW))
 
 servo.angle=0
 
 while (True):
-    respuesta=input("Cerrar o abrir")
+    respuesta=input("Cerrar o abrir",min_pulse_width=minPW,max_pulse_width=maxPW)
 
     if respuesta=="Cerrar":
         servo.angle=90
