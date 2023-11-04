448c2665061904682d02f12aac361ea498091098
otac12 <otac@hotmail.com>
2023-11-03 20:16:49 -0600

    Enganche4

--- Enganche.py
+++ Enganche.py
@@ -5,14 +5,16 @@ print("Se movera motor")
 
 servo= AngularServo(17,min_pulse_width=0.0006, max_pulse_width=0.0023)
 
+servo.angle=-90
+
 while (True):
     respuesta=input("Cerrar o abrir")
 
     if respuesta=="Cerrar":
-        servo.angle=180
+        servo.angle=90
     
     if respuesta=="abrir":
-        servo.angle=0
+        servo.angle=-90
 
 
 
