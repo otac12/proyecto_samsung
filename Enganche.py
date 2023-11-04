500de95192845604c0bf1198f3501a3eac956ea3
otac12 <otac@hotmail.com>
2023-11-03 20:02:24 -0600

    Enganche4

--- Enganche.py
+++ Enganche.py
@@ -3,18 +3,18 @@ from time import sleep
 
 print("Se movera motor")
 
-servo= Servo(17)
+servo= Servo(17, min_pulse_width=1.0, max_pulse_width=2.0)
 
-servo.angle=0
+servo.value=-1
 
 while (True):
     respuesta=input("Cerrar o abrir")
 
     if respuesta=="Cerrar":
-        servo.angle=90
+        servo.value=1
     
     if respuesta=="abrir":
-        servo.angle=0
+        servo.value=0
 
 
 
