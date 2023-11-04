2110c4891dcd46d116bbbf94418eb2cf86f2832a
otac12 <otac@hotmail.com>
2023-11-03 20:13:04 -0600

    Enganche4

--- Enganche.py
+++ Enganche.py
@@ -2,16 +2,11 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=0, max_angle=-90)
+servo = AngularServo(17, min_angle=-90, max_angle=90)
 
 while True:
-    servo.angle = 0
+    servo.min()
     sleep(2)
-    servo.angle = 90
-    sleep(2)
-    servo.angle = 180
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
+    servo.max()
 
 
