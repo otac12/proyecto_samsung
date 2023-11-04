dd08583282da45adcc15be8af775748235188876
otac12 <otac@hotmail.com>
2023-11-03 18:32:25 -0600

    Enganche2

--- Enganche.py
+++ Enganche.py
@@ -5,13 +5,7 @@ print("Se movera motor")
 
 servo= AngularServo(17,min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-while (True):
-    servo.angle = 90
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
+servo.angle(0)
 
 
 
