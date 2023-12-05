94ae5301947f88a7e13d89754b0f23d19fb6a27e
otac12 <otac@hotmail.com>
2023-12-04 20:31:55 -0600

    front

---
+++ cliente/actividades/registro.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro</title>
+    <link rel="stylesheet" href="../style/registro.css" type="text/css">
+</head>
+<body>
+    <div id="registro">
+        
+    </div>
+</body>
+</html>
---
+++ cliente/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style/login.css" type="text/css">
+</head>
+<body>
+    <div id="login">
+        <div id="bolita"></div>
+        <img src="../recursos/logo.png">
+        <p>Email</p>
+        <input type="text" name="email" placeholder="  Ingresar tu email">
+        <p>Contraseña</p>
+        <input type="password" name="password" placeholder="  Ingresar tu Contraseña">
+        <Button >Ingresar</Button>
+        <a href="actividades/registro.php">¿No tienes cuenta? Registrate</a>
+    </div>
+</body>
+</html>
